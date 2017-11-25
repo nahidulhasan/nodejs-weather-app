172c0ba923cedee08d515952f886a0e480f91dd0
nahid35 <nahidul.cse@gmail.com>
2017-11-25 14:18:59 +0600

    Added a printweather method for printing result

--- weather.js
+++ weather.js
@@ -3,6 +3,15 @@ const https = require('https');
 
 const api = require('./api.json')
 
+
+function printWeather(weather) {
+
+  const message ='Current Temperature in ${weather.location.city} is ${weather.current_observation.temp_f} F';
+
+  cosole.log(message);
+
+}
+
 function get(query) {
 
 	const request = 'https://api.wunderground.com/api/${api.key}/geolookup/conditions/q/${query}.json',
@@ -16,9 +25,14 @@ function get(query) {
 		});
 
 		response.on('end', () => {
+           
+           const weather = JSON.parse(body);
 
-			cosole.log(body);
+           printWeather(weather);
+           
 		});
 	};
 
 }
+
+module.exports.get = get;
