a7469eea10e05804192956f94e8cfda644ba93a7
Nahidul Hasan <nahidul.cse@gmail.com>
2018-04-30 15:07:54 +0600

    Refactored code and modify gitignore

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const api = require('./api.json');
-const request = require('request');
+const axios = require('axios');
 
+const getWeather = async city => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api.key}`
+
+    try {
+        const response = await axios.get(url);
+
+        if(response.data.cod === 200){
+            try{
+                if(response.data.name) {
+                    return response.data;
+                } else {
+                    const queryError = new Error(`The location ${city} was not found`);
+                    printError(queryError);
+                }
+            } catch(error) {
+                printError(error);
+            }
+        } else {
+            const statusCodeError =
+                new Error(`There was an error getting the message for ${city}(StatusCode ${response.data.cod})`);
+            printError(statusCodeError);
+        }
+    } catch(error) {
+        printError(error);
+    }
+}
 
 if(require.main == module) {
     const argument = process.argv.slice(3).join(' ');
     const city = argument || 'dhaka';
 
-    get(city);
+    getWeather(city).then( val => {
+        printWeather(val);
+    });
 }
 
 function printWeather(weather) {
@@ -19,39 +47,7 @@ function printError(error) {
     console.error(error.message);
 }
 
-function get(city) {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api.key}`
-
-    try{
-        request(url, function (error, response, body) {
-
-            if(response.statusCode === 200){
-
-                try{
-                    const weather = JSON.parse(body);
-
-                    if(weather.name) {
-                        printWeather(weather);
-                    } else {
-                        const queryError = new Error(`The location ${city} was not found`);
-                        printError(queryError);
-                    }
-
-                } catch(error) {
-                    printError(error);
-                }
-
-            } else {
-                const statusCodeError =
-                    new Error(`There was an error getting the message for ${city}(StatusCode ${response.statusCode})`);
-                printError(statusCodeError);
-            }
-
-        });
-
-    } catch(error) {
-        printError(error);
-    }
-}
-
-module.exports.get = get;
+module.exports = {
+    getWeather,
+    printWeather
+};
